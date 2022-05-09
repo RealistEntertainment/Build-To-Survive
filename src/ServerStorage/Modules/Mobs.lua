cdfd9c7a6059e17bf21a0b359aa820fbf05bdafb
zKobrax <darekwywrot@gmail.com>
2022-05-09 13:04:19 -0500

    Update Mobs.lua

--- src/ServerStorage/Modules/Mobs.lua
+++ src/ServerStorage/Modules/Mobs.lua
@@ -8,7 +8,7 @@ Mobs["Giant Rat"] = {
     DeathReward = 5, --// amount of coins rewarded on death
     AttackDelay = 1.2 --// in seconds
 },
-Mobs["Green Slime"] = {
+    ["Green Slime"] = {
     DamageType = "Melee", --// not used yet
     AttackRange = 4, --// this is from the center of the model(might make some kinda of calculation to make this automated)
     ObjectDamage = 5, --// damage dealt to objects
