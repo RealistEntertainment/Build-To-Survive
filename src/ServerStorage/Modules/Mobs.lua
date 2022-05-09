3b733835982ac92b1d70a904f4f141b2147d5f7e
zKobrax <darekwywrot@gmail.com>
2022-05-09 13:14:44 -0500

    a

--- src/ServerStorage/Modules/Mobs.lua
+++ src/ServerStorage/Modules/Mobs.lua
@@ -3,18 +3,18 @@ local Mobs = {}
 Mobs["Giant Rat"] = {
     DamageType = "Melee", --// not used yet
     AttackRange = 4, --// this is from the center of the model(might make some kinda of calculation to make this automated)
-    ObjectDamage = 5, --// damage dealt to objects
-    PlayerDamage = 5, --// damage dealt to players
-    DeathReward = 5, --// amount of coins rewarded on death
+    ObjectDamage = 8, --// damage dealt to objects
+    PlayerDamage = 10, --// damage dealt to players
+    DeathReward = 25, --// amount of coins rewarded on death
     AttackDelay = 1.2 --// in seconds
-},
-    ["Green Slime"] = {
-    DamageType = "Melee", --// not used yet
-    AttackRange = 4, --// this is from the center of the model(might make some kinda of calculation to make this automated)
-    ObjectDamage = 5, --// damage dealt to objects
-    PlayerDamage = 5, --// damage dealt to players
-    DeathReward = 5, --// amount of coins rewarded on death
-    AttackDelay = 1.2 --// in seconds
+}
+Mobs["Green Slime"] = {
+	DamageType = "Melee", --// not used yet
+	AttackRange = 4, --// this is from the center of the model(might make some kinda of calculation to make this automated)
+	ObjectDamage = 5, --// damage dealt to objects
+	PlayerDamage = 8, --// damage dealt to players
+	DeathReward = 10, --// amount of coins rewarded on death
+	AttackDelay = 1.1 --// in seconds
 }
 
 return Mobs
--- src/ServerStorage/Modules/Rounds.lua
+++ src/ServerStorage/Modules/Rounds.lua
@@ -2,43 +2,46 @@ local ServerStorage = game:GetService("ServerStorage")
 local Rounds = {}
 
 Rounds.List = {
-    "rat 1"
+	"Rat1",
+	"RatSwarm",
+	"GreenSlime1",
+	"Meteor Shower 1"
 }
 
-Rounds["rat 1"] = {
+Rounds["Rat1"] = {
     NpcModel = "Giant Rat",
     EventType = "MobSpawning",
-    Announcement = "Hide 1 Giant Rat inbound!",
+    Announcement = "Hide! Three Giant Rats are on the way!",
     RoundTime  = 60,
-    Award = 20,
-    NpcCount = 1,
-}
-
-Rounds["rat 2"] = {
-    NpcModel = "Giant Rat",
-    EventType = "MobSpawning",
-    Announcement = "Hide 2 Giant Rats inbound!",
-    RoundTime  = 60,
-    Award = 20,
-    NpcCount = 2,
-}
-
-Rounds["rat 3"] = {
-    NpcModel = "Giant Rat",
-    EventType = "MobSpawning",
-    Announcement = "Hide 3 Giant Rats inbound!",
-    RoundTime  = 60,
-    Award = 20,
+    Award = 50,
     NpcCount = 3,
+}
+
+Rounds["RatSwarm"] = {
+    NpcModel = "Giant Rat",
+    EventType = "MobSpawning",
+    Announcement = "Hide! There are a lot of Giant Rats on the way!",
+    RoundTime  = 60,
+    Award = 100,
+    NpcCount = 8,
+}
+
+Rounds["GreenSlime1"] = {
+	NpcModel = "Green Slime",
+	EventType = "MobSpawning",
+	Announcement = "Hide! Three Green Slimes are on the way!",
+	RoundTime  = 60,
+	Award = 25,
+	NpcCount = 3,
 }
 
 Rounds["Meteor Shower 1"] = {
     EventType = "MeteorShower",
     Announcement = "Take cover it's raining fiery stones!",
-    Award = 20,
+    Award = 50,
     MeteorModel = "Meteor",
     Meteors = 90, --// (round time - 5)/ meteors = time delay between meteors
-    MeteorDamage = 5, --// damage dealt to touched objects
+    MeteorDamage = 25, --// damage dealt to touched objects
     RoundTime  = 60,
 }
 
